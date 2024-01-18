adb082a561fecdf1c9c9a8c7515a015cd7a45ec1
tiodot <tiodot@163.com>
2024-01-18 22:18:56 +0800

    自定义sidebar

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import fs from 'node:fs';
+import path from 'node:path';
+
+const getSidebarByDir = () => {
+  const dirConfig = [
+    {text: '算法相关', dir: 'algorithm'}, 
+    {text: '前端 Coding', dir: 'utils'}
+  ]
+  return dirConfig.map(config => {
+    const files = fs.readdirSync(path.resolve(__dirname, '..', config.dir));
+    console.log(files);
+    const filePath = files.filter(file => file.endsWith('.md')).map(name => `/${config.dir}/${name}`);
+    return {
+      text: config.text,
+      children: filePath
+    }
+  })
+}
+// console.log('----config---', getSidebarByDir())
+
+export default {
+  theme: defaultTheme({
+    // 在这里进行配置
+    sidebar: getSidebarByDir()
+  }),
+}
